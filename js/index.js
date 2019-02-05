35ddc3d7840067a11b10de939238235d1ee835bf
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 20:04:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,6 +9,10 @@ function doThings() {
   
   const input = document.querySelector('input');
   input.addEventListener('keydown', function(e){
-    console.log(e.key);
+    if(e.key === 'g') {
+      return e.preventDefault();
+    } else {
+      console.log(e.key);
+    }
   })
 }
