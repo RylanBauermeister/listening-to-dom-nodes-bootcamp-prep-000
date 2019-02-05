bec7b277b6f425a379aa11e5615e22b333b693b4
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 20:09:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -19,9 +19,8 @@ function doThings() {
   let divs = document.querySelectorAll('div');
   function bubble(e){
     console.log(this.firstChild.nodeValue.trim() + ' bubbled');
-    
-    for(let i = 0; i < divs.length; i++) {
-      divs[i].addEventListener('click', bubble);
-    }
+  }
+  for(let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
   }
 }
