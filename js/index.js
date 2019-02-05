69e7c711a91550df84ddeb002e5eb8a367d54240
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 20:11:47 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,9 +22,9 @@ function doThings() {
   }
   
   function capture(e){
-    
+    console.log(this.firstChild.nodeValue.trim() + ' captured!');
   }
   for(let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', bubble);
+    divs[i].addEventListener('click', capture, true);
   }
 }
